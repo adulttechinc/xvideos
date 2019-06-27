f2c767e6fc3bbb2bb0c8553f992a3a37323a824a
Stone Guan <stone54321277@gmail.com>
2019-06-27 18:58:47 +0900

    Fix best videos and verified videos.

--- example/example.js
+++ example/example.js
@@ -50,6 +50,60 @@ async function retrieveFreshVideos() {
     console.log('----' + JSON.stringify(dashboardList.videos[0]));
     console.log('----' + JSON.stringify(detail));
  }
+
+ async function retrieveBestVideos() {
+    const bestList = await xvideos.videos.best({ year: '2019', month: '05', page: 5 });
+    console.log(bestList.videos); // { url, path, title, duration, profile: { name, url }, views, }
+    console.log(bestList.pagination.current); // 1 
+    console.log(bestList.pagination.pages); // [1, 2, 3, 4, 5...]
+    console.log(bestList.hasNext()); // true
+    console.log(bestList.hasPrevious()); // false
+    
+    const nextPage = await bestList.next();
+    console.log(nextPage.pagination.current); // 2
+    console.log(nextPage.hasNext()); // true
+    console.log(nextPage.hasPrevious()); // true
+    
+    if (bestList.hasPrevious()) {
+     const previousPage = await bestList.previous();
+     console.log(previousPage.pagination.current); // 1
+     console.log(previousPage.hasNext()); // true
+     console.log(previousPage.hasPrevious()); // tfalse
+    }
+    
+    
+    const detail = await xvideos.videos.details(bestList.videos[0]); 
+    console.log('----' + JSON.stringify(bestList.videos[0]));
+    console.log('----' + JSON.stringify(detail));
+ }
+
+ async function retrieveVerifiedVideos() {
+    const verifiedList = await xvideos.videos.verified({ page: 2 });
+    console.log(verifiedList.videos); // { url, path, title, duration, profile: { name, url }, views, }
+    console.log(verifiedList.pagination.current); // 1 
+    console.log(verifiedList.pagination.pages); // [1, 2, 3, 4, 5...]
+    console.log(verifiedList.hasNext()); // true
+    console.log(verifiedList.hasPrevious()); // false
+    
+    const nextPage = await verifiedList.next();
+    console.log(nextPage.pagination.current); // 2
+    console.log(nextPage.hasNext()); // true
+    console.log(nextPage.hasPrevious()); // true
+    
+    if (verifiedList.hasPrevious()) {
+     const previousPage = await verifiedList.previous();
+     console.log(previousPage.pagination.current); // 1
+     console.log(previousPage.hasNext()); // true
+     console.log(previousPage.hasPrevious()); // tfalse
+    }
+    
+    
+    const detail = await xvideos.videos.details(verifiedList.videos[0]); 
+    console.log('----' + JSON.stringify(verifiedList.videos[0]));
+    console.log('----' + JSON.stringify(detail));
+ }
  
-//  retrieveFreshVideos();
- retrieveDashboardVideos();
+retrieveFreshVideos();
+retrieveDashboardVideos();
+retrieveBestVideos();
+retrieveVerifiedVideos();
